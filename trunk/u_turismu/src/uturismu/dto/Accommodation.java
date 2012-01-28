c8f921da4bd0c3365cb850aab37fa9c6e7a36b3e
giulagreca@gmail.com <giulagreca@gmail.com@3574f415-172f-496d-1c4e-3696531ded0e>
2012-01-28 16:28:13 +0000

    test commit no classpath

--- trunk/u_turismu/src/uturismu/dto/Accommodation.java
+++ trunk/u_turismu/src/uturismu/dto/Accommodation.java
@@ -7,6 +7,7 @@ public abstract class Accommodation implements Serializable {
 
 	private static final long serialVersionUID = 226753614504147949L;
 	private Long id;
+	
 	private String vatNumber; // partita IVA
 	private String name;
 	private Address address;
