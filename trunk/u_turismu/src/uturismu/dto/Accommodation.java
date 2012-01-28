b8ff483a68324f62f6fbde2c9e358dfa03fb5dc5
spa.simone@gmail.com <spa.simone@gmail.com@3574f415-172f-496d-1c4e-3696531ded0e>
2012-01-28 22:03:16 +0000

    fixed "Accommodation"

--- trunk/u_turismu/src/uturismu/dto/Accommodation.java
+++ trunk/u_turismu/src/uturismu/dto/Accommodation.java
@@ -3,14 +3,19 @@ package uturismu.dto;
 import java.io.Serializable;
 import java.util.Set;
 
-public abstract class Accommodation implements Serializable {
+import org.hibernate.annotations.Entity;
+
+import uturismu.dto.util.AccommodationType;
+
+@Entity
+public class Accommodation implements Serializable {
 
 	private static final long serialVersionUID = 226753614504147949L;
 	private Long id;
-	
 	private String vatNumber; // partita IVA
 	private String name;
 	private Address address;
+	private AccommodationType type;
 	private Set<OvernightStay> overnightsStay;
 
 }
